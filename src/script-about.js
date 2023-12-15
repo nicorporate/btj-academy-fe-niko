e4adaa15b1a8adf2740e20d130842e9a79eea08f
N.I <79742752+nicorporate@users.noreply.github.com>
2023-12-15 23:03:09 +0800

    Add files via upload

--- src/script-about.js
+++ src/script-about.js
@@ -1,5 +1,27 @@
-$(document).ready(function () {
-  $("#myButton").click(function () {
-    alert("Tombol diklik!");
+$(".logo").ready(function () {
+  $("#welcome").click(function () {
+    alert("Hi, Selamat Datang!");
   });
+});
+
+$("#profile").mouseenter(function () {
+  $(this).css("background-color", "black");
+
+  // mouseleave: function () {
+  //   $(this).css("background-color", "lightblue");
+  // },
+  // click: function () {
+  //   $(this).css("background-color", "yellow");
+  // },
+});
+
+$(document).ready(function () {
+  let rotationDegree = 0;
+
+  function rotateImage() {
+    rotationDegree += 360;
+    $("#rotating-image").css("transform", "rotate(" + rotationDegree + "deg)");
+  }
+
+  $("#rotating-image").click(rotateImage);
 });
